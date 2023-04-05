4b9157fd23ba245efd8e68177db586f9dd567373
ayaabusoud <ayaabusoud23@gmail.com>
2023-04-05 21:08:19 +0300

    Update Restaurant Home Page

--- Luqmit3ish/Luqmit3ish/Services/FoodServices.cs
+++ Luqmit3ish/Luqmit3ish/Services/FoodServices.cs
@@ -44,17 +44,7 @@ namespace Luqmit3ish.Services
             }
         }
 
-        public ObservableCollection<Dish> Dishes { get; }
-
-        public ObservableCollection<Dish> GetFoodTest()
-        {
-            return new ObservableCollection<Dish>
-            {
-                new Dish { id = 2,keep_listed =3, user_id = 3,photo="https://res.cloudinary.com/hesvvq3zo/image/upload/c_scale,w_1000/v1628074146/01t3V000000zhOuQAI/Images__c/Pizza_Strabuona.jpg.jpg",number = 5, name = "Pizza", description = "mushroom, onion, cheese", pick_up_time = new DateTime(2020, 3, 23) },
-                new Dish { id = 2,keep_listed =3, user_id = 3,photo="https://res.cloudinary.com/hesvvq3zo/image/upload/c_scale,w_1000/v1628074146/01t3V000000zhOuQAI/Images__c/Pizza_Strabuona.jpg.jpg",number = 5, name = "Cheese Pizza", description = "mushroom, onion, cheese", pick_up_time = new DateTime(2020, 3, 23) }
-
-        };
-        }
+        
 
     }
 }
