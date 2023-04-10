47c347278032b0c8182e05bd3eeb47adf5372251
ayaabusoud <ayaabusoud23@gmail.com>
2023-04-10 12:30:21 +0300

    LUQMIT3ISH-33-BuildCharityOrder

--- Luqmit3ish/Luqmit3ish/ViewModels/CharityOrderViewModel.cs
+++ Luqmit3ish/Luqmit3ish/ViewModels/CharityOrderViewModel.cs
@@ -1,29 +1,62 @@
-﻿using Luqmit3ish.Views;
+﻿using Luqmit3ish.Models;
+using Luqmit3ish.Services;
+using Luqmit3ish.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 
 namespace Luqmit3ish.ViewModels
 {
-    class CharityOrderViewModel : INotifyPropertyChanged
+    class CharityOrderViewModel : ViewModelBase
     {
         public INavigation Navigation { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand EditCommand { protected set; get; }
         public ICommand ProfileCommand { protected set; get; }
+        public OrderService orderService;
+        public ICommand DeleteCommand { protected set; get; }
+
         public CharityOrderViewModel(INavigation navigation)
         {
             this.Navigation = navigation;
             EditCommand = new Command(async () => await OnEditClicked());
             ProfileCommand = new Command(async () => await OnProfileClicked());
+            DeleteCommand = new Command(OnDeleteClicked);
+            orderService = new OrderService();
+            OnInit();
         }
+
+        private void OnDeleteClicked()
+        {
+            //imp
+        }
+
+        private ObservableCollection<OrderCard> _orderCard;
+
+        public ObservableCollection<OrderCard> OrderCard
+        {
+            get => _orderCard;
+            set => SetProperty(ref _orderCard, value);
+        }
+
+        private async void OnInit()
+        {
+            var id = Preferences.Get("userId", null);
+            var userId = int.Parse(id);
+            OrderCard = await orderService.GetOrders(userId);
+
+            
+        }
+
         private async Task OnEditClicked()
         {
             try
