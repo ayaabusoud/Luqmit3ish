2955cf9691ef1bc7eb1e2b4e4f86e7b798c08a4c
Ibtihaj7 <ibtihaj.adham7@gmail.com>
2023-04-11 17:12:11 +0300

    Design filter food screen- edit LUQMIT3ISH-69

--- Luqmit3ish/Luqmit3ish/Models/TypeField.cs
+++ Luqmit3ish/Luqmit3ish/Models/TypeField.cs
@@ -7,9 +7,9 @@ namespace Luqmit3ish.Models
 {
     public class TypeField : INotifyPropertyChanged
     {
-        public string name { get; set; }
-        public string iconText { get; set; }
-
+        public TypeFieldValue Value { get; set; }
+        public string Name { get; set; }
+        public string IconText { get; set; }
         private bool isSelected;
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -23,14 +23,16 @@ namespace Luqmit3ish.Models
                 {
                     isSelected = value;
                     OnPropertyChanged(nameof(IsSelected));
-                    OnPropertyChanged(nameof(StackLayoutBackgroundColor));
-                    OnPropertyChanged(nameof(StackLayoutBackgroundColor1));
+                    OnPropertyChanged(nameof(BackgroundColor));
+                    OnPropertyChanged(nameof(FrameBackgroundColor));
+                    OnPropertyChanged(nameof(TextBackgroundColor));
                 }
             }
         }
 
-        public Color StackLayoutBackgroundColor => IsSelected ? Color.White : Color.White; 
-        public Color StackLayoutBackgroundColor1 => IsSelected ? Color.FromArgb(0x4D, 0x6B, 0xA3, 0xFF) : Color.White;
+        public Color BackgroundColor => IsSelected ? Color.FromArgb(237, 242, 245) : Color.Transparent;
+        public Color FrameBackgroundColor => IsSelected ? Color.FromArgb(0x4D, 0x6B, 0xA3) : Color.FromArgb(0x4D, 0x6B, 0xA3, 0xFF);
+        public Color TextBackgroundColor => IsSelected ? Color.White : Color.FromArgb(0x4D, 0x6B, 0xA3);
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
