80ceace69c9fa264053c35218143336165f4ee27
Karam R. Hazza <77400199+william94411@users.noreply.github.com>
2023-04-12 21:33:57 +0300

    LUQMIT3ISH-40 build restaurant orders

--- Luqmit3ish/Luqmit3ish/ViewModels/RestaurantOrderViewModel.cs
+++ Luqmit3ish/Luqmit3ish/ViewModels/RestaurantOrderViewModel.cs
@@ -1,31 +1,115 @@
-﻿using Luqmit3ish.Views;
+using Luqmit3ish.Models;
+using Luqmit3ish.Services;
+using Luqmit3ish.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.PancakeView;
 
 namespace Luqmit3ish.ViewModels
 {
-    class RestaurantOrderViewModel : INotifyPropertyChanged
+    class RestaurantOrderViewModel : ViewModelBase
     {
         public INavigation Navigation { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
         public ICommand ProfileCommand { protected set; get; }
+        public OrderService orderService;
+        public FoodServices foodService;
+        public ICommand DoneCommand { protected set; get; }
+
+
+
+
+        private ObservableCollection<Dish> _dishes;
+
+        public ObservableCollection<Dish> Dishes
+        {
+            get => _dishes;
+            set => SetProperty(ref _dishes, value);
+        }
         public RestaurantOrderViewModel(INavigation navigation)
         {
             this.Navigation = navigation;
-            ProfileCommand = new Command(async () => await OnProfileClicked());
+            ExpanderCommand = new Command<int>(OnExpanderClicked);
+            orderService = new OrderService();
+            foodService = new FoodServices();
+            DoneCommand = new Command(OnDoneClick);
+            OnInit();
         }
+
+        private void OnDoneClick(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool _isExpanded = false;
+
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set => SetProperty(ref _isExpanded, value);
+        }
+        public Command<int> ExpanderCommand { protected set; get; }
+        private void OnExpanderClicked(int id)
+        {
+            var item = OrderCard.FirstOrDefault(i => i.id == id);
+            if (item != null)
+            {
+                if (item.IsExpanded)
+                {
+                    item.IsExpanded = false;
+                }
+                else
+                {
+                    item.IsExpanded = true;
+                }
+            }
+
+        }
+    
+
+       
+
+
+        private ObservableCollection<OrderCard> _orderCard;
+
+        public ObservableCollection<OrderCard> OrderCard
+        {
+            get => _orderCard;
+            set => SetProperty(ref _orderCard, value);
+        }
+
+        private async void OnInit()
+        {
+            var id = Preferences.Get("userId", null);
+            var userId = int.Parse(id);
+            OrderCard = await orderService.GetRestaurantOrders(userId,false);
+            if (OrderCard is null)
+            {
+                //no orders yet
+            }
+
+
+        }
+
+       
         private async Task OnProfileClicked()
         {
+
             try
             {
-            await Navigation.PushAsync(new OtherProfilePage(1));
+                await Navigation.PushAsync(new OtherProfilePage(1));
 
             }
             catch (ArgumentException e)
