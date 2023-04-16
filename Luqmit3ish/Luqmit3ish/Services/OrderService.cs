5011397b7ac23e2efd14fb72f406eec9549d9c42
Karam R. Hazza <77400199+william94411@users.noreply.github.com>
2023-04-16 10:35:57 +0300

    LUQMIT3ISH-41 Build received orders

--- Luqmit3ish/Luqmit3ish/Services/OrderService.cs
+++ Luqmit3ish/Luqmit3ish/Services/OrderService.cs
@@ -183,6 +183,42 @@ namespace Luqmit3ish.Services
 
 
         }
+         public async Task<bool> UpdateOrderReceiveStatus(int id)
+        {
+            if (!_connection.CheckInternetConnection())
+            {
+                throw new ConnectionException("There is no internet connection");
+            }
+            try
+            {
+
+                var patchObject = new { id };
+                var patchData = JsonConvert.SerializeObject(patchObject);
+                var httpContent = new StringContent(patchData, Encoding.UTF8, "application/json");
+
+                var request = new HttpRequestMessage(new HttpMethod("PATCH"), _orderApiUrl + "/" + id + "/" + "receive")
+                {
+                    Content = httpContent
+                };
+
+                var response = await _httpClient.SendAsync(request);
+
+                return response.IsSuccessStatusCode;
+
+            }
+            catch (HttpRequestException e)
+            {
+                throw new HttpRequestException(e.Message);
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+
+
+            }
+
+        }
     }
 }
 
