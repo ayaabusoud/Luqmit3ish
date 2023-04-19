51f2db67549fe2b45b8187bdc3359055ecb24b52
AyasSharqawe <92643395+AyasSharqawe@users.noreply.github.com>
2023-04-19 14:16:33 +0300

    update foodDetails page

--- Luqmit3ish/Luqmit3ish/ViewModels/FoodDetailViewModel.cs
+++ Luqmit3ish/Luqmit3ish/ViewModels/FoodDetailViewModel.cs
@@ -16,36 +16,25 @@ using Xamarin.Forms;
 
 namespace Luqmit3ish.ViewModels
 {
-    class FoodDetailViewModel: ViewModelBase
+    class FoodDetailViewModel : ViewModelBase
     {
-    private INavigation _navigation { get; set; }
+        private INavigation _navigation { get; set; }
         public Command<int> PlusCommand { protected set; get; }
         public ICommand MinusCommand { protected set; get; }
         public Command<int> ReserveCommand { protected set; get; }
         public Command<int> ProfileCommand { protected set; get; }
         private FoodServices _foodServices;
-        private UserServices _userServices;
-        private OrderService _orderService;
 
-        private int _counter = 0;
         private bool _isEnabled = false;
+
         public bool IsEnabled
         {
             get => _isEnabled;
             set => SetProperty(ref _isEnabled, value);
         }
-        private String _restaurantImg;
-        public String RestaurantImg
-        {
-            get => _restaurantImg;
-            set => SetProperty(ref _restaurantImg, value);
-        }
-        private String _restaurantName;
-        public String RestaurantName
-        {
-            get => _restaurantName;
-            set => SetProperty(ref _restaurantName, value);
-        }
+
+        private int _counter = 0;
+
         public int Counter
         {
             get => _counter;
@@ -68,36 +57,7 @@ namespace Luqmit3ish.ViewModels
             get => _dish;
             set => SetProperty(ref _dish, value);
         }
-        private int _quantity;
-        public int Quantity
-        {
-            get => _quantity;
-            set => SetProperty(ref _quantity, value);
-        }
-        private String _image;
-        public String Image
-        {
-            get => _image;
-            set => SetProperty(ref _image, value);
-        }
-        private int _keepValid;
-        public int KeepValid
-        {
-            get => _keepValid;
-            set => SetProperty(ref _keepValid, value);
-        }
-        private String _description;
-        public String Description
-        {
-            get => _description;
-            set => SetProperty(ref _description, value);
-        }
-        private String _pickUp;
-        public String PickUp
-        {
-            get => _pickUp;
-            set => SetProperty(ref _pickUp, value);
-        }
+
         private void OnMinusClicked()
         {
             if (Counter == 0)
@@ -123,7 +83,6 @@ namespace Luqmit3ish.ViewModels
             {
                 Counter++;
             }
-
         }
         private async Task OnProfileClicked(int restaurantId)
         {
@@ -150,9 +109,9 @@ namespace Luqmit3ish.ViewModels
         }
         private async Task OnReserveClicked(int FoodId)
         {
-            
+
         }
-        public FoodDetailViewModel (int id , INavigation navigation)
+        public FoodDetailViewModel(int id, INavigation navigation)
         {
             this._navigation = navigation;
             ProfileCommand = new Command<int>(async (int restaurantId) => await OnProfileClicked(restaurantId));
@@ -160,8 +119,6 @@ namespace Luqmit3ish.ViewModels
             MinusCommand = new Command(OnMinusClicked);
             ReserveCommand = new Command<int>(async (int FoodId) => await OnReserveClicked(FoodId));
             _foodServices = new FoodServices();
-            _orderService = new OrderService();
-            _userServices = new UserServices();
             OnInit(id);
         }
 
@@ -184,22 +141,7 @@ namespace Luqmit3ish.ViewModels
             {
                 Debug.WriteLine(e.Message);
             }
-            if(Dish != null)
-               
-            {
-                
-                PickUp = Dish[0].pickUpTime;
-                Image = Dish[0].photo;
-                Description = Dish[0].description;
-                KeepValid = Dish[0].keepValid;
-                Quantity = Dish[0].quantity;
-                RestaurantImg = Dish[0].RestaurantImage;
-                RestaurantName = Dish[0].restaurantName; 
-            }
-            if(RestaurantImg == null || RestaurantImg == "")
-            {
-                RestaurantImg = "Luqma.jpg"; 
-            }
+
 
         }
     }
