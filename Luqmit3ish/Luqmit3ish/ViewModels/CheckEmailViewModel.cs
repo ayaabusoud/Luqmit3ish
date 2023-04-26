ac4b1fde57a1823614ad6dca7eaabff23b0ca3d7
Karam47 <77400199+william94411@users.noreply.github.com>
2023-04-26 11:33:59 +0300

    LUQMIT3ISH-192 bug fixes

--- Luqmit3ish/Luqmit3ish/ViewModels/CheckEmailViewModel.cs
+++ Luqmit3ish/Luqmit3ish/ViewModels/CheckEmailViewModel.cs
@@ -1,12 +1,16 @@
+using Luqmit3ish.Exceptions;
+using Luqmit3ish.Services;
 using Luqmit3ish.Views;
+using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Luqmit3ish.ViewModels
@@ -14,22 +18,69 @@ namespace Luqmit3ish.ViewModels
     class CheckEmailViewModel : ViewModelBase
     {
         private INavigation _navigation { get; set; }
-        private string _email;
 
+        private string verificationCode;
+
+        private EmailService _emailService;
         public ICommand ResetCommand { protected set; get; }
-        public CheckEmailViewModel(INavigation navigation, string email)
+        public CheckEmailViewModel(INavigation navigation, string Email)
         {
-            _email = email;
             this._navigation = navigation;
-            ResetCommand = new Command(OnResetClicked);
-
+            ResetCommand = new Command(() => OnContinueClicked(Email));
+            _emailService = new EmailService();
+            OnInit(Email);
         }
 
-        private void OnResetClicked()
+        private int _pin;
+        public int PIN
+        {
+            get => _pin;
+            set => SetProperty(ref _pin, value);
+        }
+
+        private void OnInit(string Email)
+        {
+            Task.Run(async () => {
+                try
+                {
+                    verificationCode = await _emailService.SendVerificationCode("", Email);
+
+                }
+                catch (ConnectionException e)
+                {
+                    Debug.WriteLine(e.Message);
+                    await PopupNavigation.Instance.PushAsync(new PopUp("Please Check your internet connection."));
+                    Thread.Sleep(3000);
+                    await PopupNavigation.Instance.PopAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    Debug.WriteLine(e.Message);
+                    await PopupNavigation.Instance.PushAsync(new PopUp("Something went wrong, please try again."));
+                    Thread.Sleep(3000);
+                    await PopupNavigation.Instance.PopAsync();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                    await PopupNavigation.Instance.PushAsync(new PopUp("Something went wrong, please try again."));
+                    Thread.Sleep(3000);
+                    await PopupNavigation.Instance.PopAsync();
+                }
+            }).Wait();
+        }
+
+        private void OnContinueClicked(string Email)
         {
             try
             {
-                 Application.Current.MainPage = new ResetPasswordForgetPage(_email);
+                var code = int.Parse(verificationCode);
+                Console.WriteLine(code);
+                if (code == PIN)
+                {
+                    Application.Current.MainPage = new ResetPasswordForgetPage(Email);
+                }
+
 
             }
             catch (ArgumentException e)
@@ -41,5 +92,7 @@ namespace Luqmit3ish.ViewModels
                 Debug.WriteLine(e.Message);
             }
         }
+
     }
+
 }
